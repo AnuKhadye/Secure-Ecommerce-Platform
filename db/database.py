ce39da9d196037af199f84b70bf9c5e061862d7c
AnuKhadye <anuragkhadye13@gmail.com>
2025-10-29 20:55:28 +0000

    Database Connections Fix

--- db/database.py
+++ db/database.py
@@ -1,15 +1,21 @@
 import sqlite3
+import os
 
 class Database:
-    def __init__(self, db_path = 'database.db'):
+    def __init__(self, db_path = 'database.db', schema_path = 'schema.sql'):
         self.db_path = db_path
+        self.schema_path = schema_path
+
+        if not os.path.exists(self.db_path):
+            print("Database not found.\nCreating a new one...")
+            self.initialize_database()
 
     def connect(self):
         conn = sqlite3.connect(self.db_path)
         conn.row_factory = sqlite3.Row
         return conn
     
-    def initialize_database(self, schema_path = 'schema.sql'):
+    def initialize_database(self, schema_path = 'db/schema.sql'):
         conn = self.connect()
         with open(schema_path) as f:
             conn.executescript(f.read())
@@ -21,11 +27,11 @@ class Database:
 
     # PRODUCTS section
 
-    def add_product(self, name, price):
+    def add_product(self, name, price, description):
         conn = self.connect()
         conn.execute(
-            "INSERT INTO products (name, price) VALUES (?, ?)",
-            (name, price)
+            "INSERT INTO products (name, price, description) VALUES (?, ?, ?)",
+            (name, price, description)
         )
         
         conn.commit()
@@ -48,3 +54,25 @@ class Database:
         
         conn.close()
         return [dict(row) for row in products]
+    
+    # USER Section
+
+    def add_user(self, username, password):
+        with self.connect() as conn:
+            conn.execute(
+                "INSERT INTO users (username, password) VALUES (?, ?)",
+                (username, password)
+            )
+            conn.commit()
+        print(f"User created {username}")
+
+
+    def show_all_users(self):
+        with self.connect() as conn:
+            users = conn.execute(
+                "SELECT username FROM users"
+            ).fetchall()
+            conn.commit()
+
+        return [row["username"]for row in users ]
+
