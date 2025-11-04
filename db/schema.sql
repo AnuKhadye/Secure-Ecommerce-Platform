988399acfdac831cb3734f03fd31f7a6029277b1
AnuKhadye <anuragkhadye13@gmail.com>
2025-11-04 02:53:31 +0000

    adding email in schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,13 @@
+DROP TABLE IF EXISTS users;
+
+
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT UNIQUE NOT NULL,
-    password TEXT NOT NULL
-    role TEXT DEFAULT 'user'
+    password TEXT NOT NULL,
+    email TEXT NOT NULL,
+    role TEXT DEFAULT 'user',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS products (
