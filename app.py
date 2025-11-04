ccadee2c86a02e1e596d1049213f1bce9a93de38
AnuKhadye <anuragkhadye13@gmail.com>
2025-11-04 02:54:28 +0000

    managing forms(Login and Register)

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from forms import RegisterForm, LoginForm
 from db.database import Database
 
 app = Flask(__name__)
+app.secret_key = 'SecretKey'
 db = Database()
 
 @app.route('/')
@@ -36,13 +38,38 @@ def remove_from_cart(product_id):
 def checkout():
     return render_template('checkout.html')
 
-@app.route('/login')
+@app.route('/login', methods = ['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = db.validate_user(form.username.data, form.password.data)
+        if user:
+            session['user_id'] = user['id']
+            session['username'] = user['username']
+            flash("Login Sucessful", "sucess")
+            return redirect(url_for('shop'))
+        else:
+            flash("Invalid username or password", "danger")
+    return render_template('login.html', form=form)
 
-@app.route('/register')
+@app.route('/register', methods = ['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    form = RegisterForm()
+    if form.validate_on_submit():
+        existing_user = db.get_user_by_username(form.username.data)
+        if existing_user:
+            flash("Username already taken", "danger")
+            return redirect(url_for('register'))
+        db.add_user(form.username.data, form.password.data)
+        flash("Registration sucessful. Please log In.", "sucess")
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+@app.route('/logout')
+def logout():
+    session.clear(0)
+    flash("You have been logged out.", "info")
+    return render_template(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
