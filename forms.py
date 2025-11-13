ddc3593bfcfdeb2ed31265b8c42d769ab0a75fc1
AnuKhadye <anuragkhadye13@gmail.com>
2025-11-13 16:16:44 +0000

    Using WTForms for email and adding a new form for set_username

--- forms.py
+++ forms.py
@@ -1,14 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, EqualTo, Length
+from wtforms.validators import DataRequired, EqualTo, Length, Email
 
 class RegisterForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=25)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=8)])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
+
+class SetUserName(FlaskForm):
+    username = StringField('Choose a username', validators=[DataRequired(), Length(min=3, max=25)])
+    SubmitField = SubmitField('Save Username')
